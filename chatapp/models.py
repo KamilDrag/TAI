5586af6e5c31277a71d4d3c2e2276090e2fe757e
Kamil Drag <kamildrag314@gmail.com>
2016-05-05 18:08:22 +0100

    Chat is unavailable for unauthenticated users

--- chatapp/models.py
+++ chatapp/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from datetime import datetime
+from django.contrib.auth.models import User
 
 
 class Message(models.Model):
 
-    nick = models.CharField(max_length=20)
-    #message_date = models.DateTimeField(default=datetime.now())
+    nick = models.ForeignKey(User)
     message_date = models.DateTimeField(auto_now=True)
     message_content = models.CharField(max_length=80)
 
@@ -14,3 +13,17 @@ class Message(models.Model):
 
     def print_message(self):
         return "[" + str(self.message_date).split(sep='.')[0] + "] " + str(self.nick) + ": " + self.message_content
+
+    @staticmethod
+    def create_message(request):
+        if request.method == "POST":
+            if not request.user.is_anonymous():
+                content = request.POST.get('msgbox', None)
+                Message.objects.create(nick=request.user, message_content=content)
+                return True
+        return False
+
+    @staticmethod
+    def get_all():
+        return Message.objects.order_by('message_date')
+
--- chatapp/views.py
+++ chatapp/views.py
@@ -2,44 +2,22 @@ from django.shortcuts import render, render_to_response, RequestContext
 import django.contrib.auth as auth
 from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from .models import Message
-from .forms import MessageForm
 
-#def message(request):
-#    message_list = Message.objects.order_by('message_date')
-#    context = {'message_list': message_list}
-#    return render(request, 'message.html', context)
 
 def chat(request):
-    form = MessageForm(request.POST or None)
-    message_list = Message.objects.order_by('message_date')
-    if request.method == "POST":
-        if request.user.is_authenticated():
-            content = request.POST.get("message_content")
-            Message.objects.create(nick=request.user, message_content=content)
-        else:
-            return HttpResponse("You cannot send message until you are logged.")
+    if request.user.is_authenticated():
+        message_list = Message.get_all()
+        return render_to_response("chat.html", locals(), context_instance=RequestContext(request))
+    else:
+        return login(request)
 
-    return render_to_response("chat.html", locals(), context_instance=RequestContext(request))
 
 def post(request):
-    if request.method == "POST":
+    if Message.create_message(request):
         msg = request.POST.get('msgbox', None)
-        if msg is None:
-            msg = "..."
-        m = Message(nick=request.user, message_content=msg)
-        m.save()
-        return JsonResponse({ 'msg': msg, 'user': str(request.user) })
+        return JsonResponse({'msg': msg, 'user': request.user})
     else:
-        return HttpResponse('Request must be POST.')
-
-
-#def message(request):
-#    form = MessageForm(request.POST or None)
-#    message_list = Message.objects.order_by('message_date')
-#    if form.is_valid():
-#        save_it = form.save(commit=False)
-#        save_it.save()
-#    return render_to_response("message.html", locals(), context_instance=RequestContext(request))
+        return HttpResponse("Cos sie popsulo")
 
 
 def login(request):
@@ -49,16 +27,13 @@ def login(request):
         password = request.POST['password']
         user = auth.authenticate(username=username, password=password)
 
-        if user is not None:
-            if user.is_active:
-                auth.login(request, user)
-                return HttpResponseRedirect(next)
-            else:
-                return HttpResponse("Account is not active at the moment.")
-        #else:
-            #return HttpResponseRedirect(settings.LOGIN_URL)
+        if user and user.is_active:
+            auth.login(request, user)
+            return HttpResponseRedirect(next)
+        else:
+            return HttpResponse("Account is not active at the moment.")
     return render(request, "login.html", {'next': next})
 
+
 def messages(request):
-    message_list = Message.objects.order_by('message_date')
-    return render(request, 'messages.html', {'message_list': message_list})
+    return render(request, 'messages.html', {'message_list': Message.get_all()})
